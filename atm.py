e157ba41f3116421d00149c2dbd14e1644050b52
kimjisooooo <9729935@naver.com>
2025-01-08 18:47:03 +0900

    ✨ 출금 기능 추가
    사용자가 출금 금액 입력하고 잔액에서 차감되는 기능 추가, 잔액보다 큰 금액을 출금 할 시 잔액 만큼만 출금되도록 처리

--- atm.py
+++ atm.py
@@ -1,6 +1,7 @@
-#입금한 금액을 받는 변수 : deposit_amount
-#입금된 금액은 balance 변수에 추가되도록 코드를 작성
-#영수증에 다음 순서로 값이 들어가도록 코드를 만들어주세요 -> "입금", 입금 요청액, 총액 순으로 데이터 넣어주세요
+#출금 요청을 받는 변수 : withdraw_amount
+#출금 요청을한 금액을 balance 변수에뺀 금액이되도록 코드를 작성
+#영수증에 다음 순서로 값이 들어가도록 코드를 만들어주세요 -> ("출금", withdraw, balance) 순으로 데이터 넣어주세요
+#가지고 있는 금액보다 출금을 원하는 금액이 클 때 가지고있는 금액만 출금 되어야 합니다.
 #단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
 #영수증 변수는 : recipts
 
@@ -17,5 +18,12 @@ while True:
         balance = balance + deposit_amount
         receipts.append(("입금", deposit_amount, balance))
         print(f"입금하신 금액은 {deposit_amount}원 이고 현재 잔액은 {balance}원 입니다.")
+    if num == '2':
+        withdraw_amonut = int(input("출금할 금액을 입력해주세요 : "))
+        withdraw_amonut = min(balance, withdraw_amonut)
+        balance -= withdraw_amonut 
+        receipts.append(("출금", withdraw_amonut, balance))
+        print(f"출금하신 금액은 {withdraw_amonut}원 이고 현재 잔액은 {balance}원 입니다.")
+
 
 print(f"서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.")
